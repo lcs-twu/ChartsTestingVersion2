ca8928e493bafbb903bcee3a7f0ecc8500114818
Tom Wu <twu@lcs.on.ca>
2024-01-17 11:12:19 -0500

    Initial Commit

---
+++ ChartsTestingVersion2/ChartsTestingVersion2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsTestingVersion2App.swift
+//  ChartsTestingVersion2
+//
+//  Created by Tom Wu on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct ChartsTestingVersion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartsTestingVersion2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChartsTestingVersion2
+//
+//  Created by Tom Wu on 2024-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
