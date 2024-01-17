f792d72b14c4bc4c4a5c28bdf62b291fff7bd47e
Tom Wu <twu@lcs.on.ca>
2024-01-17 11:13:46 -0500

    set value for coffees

---
+++ ChartsTestingVersion2/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ChartsTestingVersion2
+//
+//  Created by Tom Wu on 2024-01-17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
